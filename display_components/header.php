935a19b2c47fe53395b8b6a14e81b49ec3ac345b
jon2502 <jonbjarke25@outlook.dk>
2024-06-20 12:38:27 +0200

    header setup

---
+++ display_components/header.php
@@ -0,0 +1,12 @@
+<section class="header">
+        <div id="header_content">
+        <img src="https://support.ipw.dk/getfile.php?objectid=247138" alt="ipw_logo" id="header_logo">
+        <button id ="phonebutton"> test </button>
+        </div>
+        <div id="links">
+                <a href="./Characters.php">Characters</a>
+                <a href="./Gallery.php">Gallery</a>
+                <a href="./Systems.php">Systems</a>
+                <a href="./downloads.php">Downloads</a>
+        </div>
+</section>
